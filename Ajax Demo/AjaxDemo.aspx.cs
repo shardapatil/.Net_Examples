e8675a30610a01175a6c66e448066cc11f4d524d
Sharda <53011896+shardapatil@users.noreply.github.com>
2023-06-15 23:46:03 +0530

    Add files via upload

---
+++ Ajax Demo/AjaxDemo.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ODL_Bootcamp
+{
+    public partial class AjaxDemo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnDisplay_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread.Sleep(3000);
+            lblResult.Text = System.DateTime.Now.ToString();
+
+        }
+    }
+}
