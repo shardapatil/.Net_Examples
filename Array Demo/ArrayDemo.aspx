acfaa0df3af9913874c3a84fab41409b84a3da8e
Sharda <53011896+shardapatil@users.noreply.github.com>
2023-06-16 21:34:57 +0530

    Add files via upload

---
+++ Array Demo/ArrayDemo.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ArrayDemo.aspx.cs" Inherits="ODL_Bootcamp.ArrayDemo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="lblArray" runat="server" Text="Enter the Array : "></asp:Label>
+        <asp:TextBox ID="txtArray" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="btnAdd" runat="server" Text="Add" OnClick="btnAdd_Click" />
+        
+        <asp:Button ID="btnDisp" runat="server" Text="Display" OnClick="btnDisp_Click" />
+        <br />
+        <br />
+        <asp:Label ID="lblResult" runat="server" Text=""></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Array Demo/ArrayDemo.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ODL_Bootcamp
+{
+    public partial class ArrayDemo : System.Web.UI.Page
+    {
+        string[] intArray = new string[10];
+        int n=0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["array"] != null)
+            {
+                intArray = Session["array"] as string[];
+                n = Convert.ToInt32(Session["count"]);
+            }
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            intArray[n++] = txtArray.Text;
+            Session["array"] = intArray;
+            Session["count"] = n;
+           
+        }
+
+        protected void btnDisp_Click(object sender, EventArgs e)
+        {
+            
+            foreach (string i in intArray)
+            {
+                lblResult.Text += i + "<br>";
+            }
+        }
+    }
+}
