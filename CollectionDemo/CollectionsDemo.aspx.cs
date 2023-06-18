e28e2030941f4f04c3ef01f9c44ed2da644244a2
Sharda <53011896+shardapatil@users.noreply.github.com>
2023-06-18 20:04:25 +0530

    Add files via upload

---
+++ CollectionDemo/CollectionsDemo.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ODL_Bootcamp
+{
+    public partial class CollectionsDemo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCollection_Click(object sender, EventArgs e)
+        {
+            List<int> lst = new List<int>();
+            lst.Add(100);
+            lst.Add(200);
+            lst.Add(300);
+            lst.Add(400);
+
+            foreach (int i in lst)
+            {
+                lblList.Text += i.ToString() + "<br>";
+            }
+        }
+    }
+}
