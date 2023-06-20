01278e281b5f14cd6bdde7cdf3fb6cf5a3b7772d
Sharda <53011896+shardapatil@users.noreply.github.com>
2023-06-20 15:44:33 +0530

    Update LeapYearDemo.aspx.cs

--- LeapYearCheck Demo/LeapYearDemo.aspx.cs
+++ LeapYearCheck Demo/LeapYearDemo.aspx.cs
@@ -11,7 +11,6 @@ namespace ODL_Bootcamp
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
         }
 
         protected void btnDispYear_Click(object sender, EventArgs e)
