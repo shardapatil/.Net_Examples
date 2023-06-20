87d2e048e5ecdd123f7fa64b8c71d69308846b10
Sharda <53011896+shardapatil@users.noreply.github.com>
2023-06-20 15:43:36 +0530

    Add files via upload

---
+++ LeapYearCheck Demo/LeapYearDemo.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ODL_Bootcamp
+{
+    public partial class LeapYearDemo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnDispYear_Click(object sender, EventArgs e)
+        {
+            int yearValue = Convert.ToInt32(txtYear.Text);
+
+            if (yearValue % 400 == 0 || yearValue % 4 == 0)
+            {
+                lblResult.Text = "Leap Year";
+            }
+            else
+            {
+                lblResult.Text = "Not a Leap Year";
+            }
+        }
+    }
+}
