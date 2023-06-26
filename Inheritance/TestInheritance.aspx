dcdbd8b38576e5204759b3dc0a0d8bc4f724e615
Sharda <53011896+shardapatil@users.noreply.github.com>
2023-06-26 22:55:16 +0530

    Add files via upload

---
+++ Inheritance/TestInheritance.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TestInheritance.aspx.cs" Inherits="ODL_Bootcamp.TestInheritance" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Button ID="btnDisp" runat="server" Text="Display Details" OnClick="btnDisp_Click" />
+        <br />
+        <br />
+        <asp:Label ID="lblSalary" runat="server" Text=""></asp:Label>
+        <br />
+        <asp:Label ID="lblBonus" runat="server" Text=""></asp:Label>
+        <br />
+        <br />
+        <asp:Button ID="btnGenerate" runat="server" Text="Generate Count" OnClick="btnGenerate_Click" />
+        <br />
+        <br />
+        <asp:Label ID="lblCount" runat="server" Text=""></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Inheritance/TestInheritance.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ODL_Bootcamp.BAL;
+
+namespace ODL_Bootcamp
+{
+    public partial class TestInheritance : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnDisp_Click(object sender, EventArgs e)
+        {
+            Programmer obj = new Programmer();
+            lblSalary.Text = "Salary of the Employee : " + obj.salary.ToString();
+            lblBonus.Text = "Bonus of the Employee : " + obj.bonus.ToString();
+
+        }
+
+        protected void btnGenerate_Click(object sender, EventArgs e)
+        {
+            ClassC obj = new ClassC();
+            lblCount.Text = "Count Generated : " + obj.count.ToString();
+        }
+    }
+}
