52d2f36ea4e4ebfe234cdf3a74d0319dc6cf39a8
Sharda <53011896+shardapatil@users.noreply.github.com>
2023-07-29 19:39:39 +0530

    Create Program.cs

---
+++ Non-Genericsclass/Program.cs
@@ -0,0 +1,38 @@
+
+namespace Session
+{
+    public class Program
+    {
+        public object data;
+
+        public string process(object input)
+        {
+            
+            if (data == input)
+            
+                return "Same"; 
+
+            else
+
+                return "Not Same";
+            
+            return null;
+        }
+
+        class TestProgram
+        {
+            static void Main(string[] args)
+            {
+
+                Program p = new Program();
+                p.data = 32;
+                Console.WriteLine(p.process(32));
+
+                Program pl = new Program();
+                pl.data = "Apple";
+                Console.WriteLine(pl.process("Apple"));
+
+            }
+        }
+    }
+}
